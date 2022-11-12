cbe5572c4f34409bcdd9f73843d5d265a6f14849
skshm <eminate894567@protonmail.com>
2022-11-12 11:20:21 +0530

    Beta Revision

--- lib/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const greenDark = Color(0xFF576B46);
-const greenLight = Color(0xFF8EA97F);
-const pink = Color(0xFFE1D8E8);
-const red = Color(0xFFC1666B);
-const blue = Color(0xFF77B6EA);
-
-const double defaultPadding = 16.0;
